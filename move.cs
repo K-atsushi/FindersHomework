a766d35c4e678dd9bbd3a72fb5b7f38d60e957c3
LAPTOP-ALH8NUMC\kawakubo atsushi <atsushi5382a@yahoo.co.jp>
2017-06-30 16:36:19 +0900

    Findersの宿題6/30提出予定

---
+++ move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Vector3 speed = new Vector3(0f, 0f, 0f);
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 speed = new Vector3(0f, 0f, 0f);
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            speed.x = 0.5f;
+            if (transform.position.x > 5.0f)
+            {
+                speed.x = 0f;
+            }
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            speed.x = -0.5f;
+            if (transform.position.x < -5.0f)
+            {
+                speed.x = 0f;
+            }
+        }
+       
+        transform.Translate(speed.x, 0f, 0f);
+        speed.x *= 0.98f;
+    }
+}
+
+
+
+
